89be7a8a4987e1db21ae49fa607ee612967805a9
capp98 <54820067+capp98@users.noreply.github.com>
2023-03-20 16:31:41 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dicio-italiano
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-msmgnu)
---
+++ script.js
@@ -0,0 +1,9 @@
+// const url = 'https://www.dizionario-italiano.it';
+// const urn = 'dizionario-italiano.php';
+
+// const af =
+//   'https://www.dizionario-italiano.it/dizionario-italiano.php?parola=persona';
+
+// const uri = url + '/' + urn;
+// const html = await fetch(af); // html as text
+// // const doc = new DOMParser().parseFromString(html, 'text/html');
